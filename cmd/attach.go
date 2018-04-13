a21d5014dbbf2efbdc7823cbed98fe1c1c1fe74e
wagnzhihaiSF <1599315665@qq.com>
2018-04-13 12:52:31 +0800

    keep one space between symbol and character

--- cmd/attach.go
+++ cmd/attach.go
@@ -22,11 +22,10 @@ import (
 
 // attachCmd represents the attach command
 var attachCmd = &cobra.Command{
-	Use:   "attach",
+	Use: "attach",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
-
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
