f5aabba3babdb738935c620bd494fbe87197d409
wagnzhihaiSF <1599315665@qq.com>
2018-04-14 17:38:09 +0800

    modify variable name

--- mock-libs/block/username.go
+++ mock-libs/block/username.go
@@ -4,13 +4,13 @@ import (
 	"github.com/iost-official/Go-IOS-Protocol/iosbase/debug"
 )
 
-func (this *NameType) set(s string) {
-	length := len(s)
+func (this *NameType) set(str string) {
+	length := len(str)
 	debug.assert(length <= 15, "Name is too long")
-	for c := range s {
+	for c := range str {
 		debug.assert(debug.assert(this.is_valid_char(c)), "Invalid char")
 	}
-	this.name = s
+	this.name = str
 }
 
 func (this *NameType) is_valid_char(c rune) {
