a3ac77739b6fd6d8b3cc583b7cc7cb68a01b0263
wagnzhihaiSF <1599315665@qq.com>
2018-04-18 13:34:14 +0800

    add mclock.go

---
+++ common/mclock/mclock.go
@@ -0,0 +1,16 @@
+package mclock
+
+import "time"
+
+package mclock
+
+import (
+"github.com/aristanetworks/goarista/monotime"
+"time"
+)
+
+type AbsTime time.Duration // absolute monotonic time
+
+func Now() AbsTime {
+	return AbsTime(monotime.Now())
+}
