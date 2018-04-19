adc33f0e26ef62a97cfa9c698872f402f000f283
wagnzhihaiSF <1599315665@qq.com>
2018-04-19 11:40:51 +0800

    update member_test.go

--- core/member_test.go
+++ core/member_test.go
@@ -15,6 +15,10 @@ func TestMember(t *testing.T) {
 			So(len(m.Pubkey), ShouldEqual, 33)
 			So(len(m.Seckey), ShouldEqual, 32)
 		})
-		
+		Convey("sign and verify: ", func() {
+			info := []byte("hello world")
+			sig := Sign(Sha256(info), m.Seckey)
+			So(VerifySignature(Sha256(info), m.Pubkey, sig), ShouldBeTrue)
+		})
 	})
 }
