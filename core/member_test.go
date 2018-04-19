0058430a2f14bb202d3fa0077901edc930cdf053
wagnzhihaiSF <1599315665@qq.com>
2018-04-19 11:39:52 +0800

    add member_test.go

---
+++ core/member_test.go
@@ -0,0 +1,20 @@
+package core
+
+import (
+	"testing"
+
+	. "github.com/iost-official/prototype/common"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestMember(t *testing.T) {
+	Convey("Test of Member", t, func() {
+		m, err := NewMember(nil)
+		Convey("New member: ", func() {
+			So(err, ShouldBeNil)
+			So(len(m.Pubkey), ShouldEqual, 33)
+			So(len(m.Seckey), ShouldEqual, 32)
+		})
+		
+	})
+}
