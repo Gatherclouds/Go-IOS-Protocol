4ebd207a72d9d06bb932ccb339e2fdda2d5c96a5
He Peijian <glisxx6@gmail.com>
2018-04-20 11:57:03 +0800

    delete func initConfig() from root.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,3 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -62,18 +48,4 @@ func init() {
 	RootCommand.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile) // enable ability to specify config file via flag
-	}
-
-	viper.SetConfigName(".Go-IOS-Protocol") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")            // adding home directory as first search path
-	viper.AutomaticEnv()                    // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
