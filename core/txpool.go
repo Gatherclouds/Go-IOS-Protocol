ee48e6849c7fcbf4818f8abe6d4444f9adb7b73f
deBreakr <wang.yannan@outlook.com>
2018-04-21 11:20:51 +0800

    add TxPool interface

--- core/txpool.go
+++ core/txpool.go
@@ -5,3 +5,14 @@ type Serializable interface {
 	Decode([]byte) error
 	Hash() []byte
 }
+//go:generate mockgen -destination mocks/mock_tx_pool.go -package core_mock github.com/iost-official/prototype/core TxPool
+type TxPool interface {
+	Add(tx Tx) error
+	Del(tx Tx) error
+	Updae(tx Tx) error
+	Find(txHash []byte) (Tx, error)
+	GetSlice() ([]Tx, error)
+	Has(txHash []byte) (bool, error)
+	Size() int
+	Serializable
+}
