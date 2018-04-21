02e64cfc7fdf4c003787ed816a19df797f3ac472
bitree8 <bitree8@gmail.com>
2018-04-21 11:05:45 +0800

    add two functions to tx.go

--- core/tx.go
+++ core/tx.go
@@ -11,3 +11,15 @@ func (d *TxInput) Decode(bin []byte) error {
 	_, err := d.Unmarshal(bin)
 	return err
 }
+
+func (d *TxInput) Hash() []byte {
+	return common.Sha256(d.Encode())
+}
+
+func (d *Tx) Encode() []byte {
+	bin, err := d.Marshal(nil)
+	if err != nil {
+		panic(err)
+	}
+	return bin
+}
