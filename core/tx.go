ec582dc72e5fa215bc16f9af6df1ac573876b115
bitree8 <bitree8@gmail.com>
2018-04-21 11:07:02 +0800

    add two functions to tx.go

--- core/tx.go
+++ core/tx.go
@@ -1,5 +1,7 @@
 package core
 
+import "github.com/iost-official/prototype/common"
+
 func (d *TxInput) Encode() []byte {
 	bin, err := d.Marshal(nil)
 	if err != nil {
@@ -22,4 +24,12 @@ func (d *Tx) Encode() []byte {
 		panic(err)
 	}
 	return bin
+}
+
+func (d *Tx) Decode(bin []byte) error {
+	_, err := d.Unmarshal(bin)
+	return err
+}
+func (d *Tx) Hash() []byte {
+	return common.Sha256(d.Encode())
 }
