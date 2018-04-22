3a0be6ea40bd30890dc9342532d1faa71b4e136f
tanky <itstangky@gmail.com>
2018-04-22 14:40:53 +0800

    add notes

--- core/utxo_pool.go
+++ core/utxo_pool.go
@@ -1,12 +1,10 @@
 package core
 
-import (
-	"bytes"
-	"fmt"
-	"github.com/gomodule/redigo/redis"
-	"sync"
-)
+//go:generate mockgen -destination mocks/mock_statepool.go -package core_mock -source utxo_pool.go -imports .=github.com/iost-official/prototype/core
 
+/*
+Current states of system ALERT: 正在施工，请不要调用
+*/
 type UTXOPool interface {
 	Add(state UTXO) error
 	Find(stateHash []byte) (UTXO, error)
