8d725082ed604b472c6f298b8858cf5bea78a5ee
wangzhihaiSF <1599315665@qq.com>
2018-04-22 13:59:58 +0800

    update tx_pool.go

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -1,5 +1,7 @@
 package core
 
+import "fmt"
+
 type Serializable interface {
 	Encode() []byte
 	Decode([]byte) error
@@ -17,4 +19,54 @@ type TxPool interface {
 	Serializable
 
 }
+
+type TxPoolImpl struct {
+	TxPoolRaw
+	txMap map[string]Tx
+}
+
+func NewTxPool() TxPool {
+	txp := TxPoolImpl{
+		txMap: make(map[string]Tx),
+		TxPoolRaw: TxPoolRaw{
+			Txs:    make([]Tx, 0),
+			TxHash: make([][]byte, 0),
+		},
+	}
+	return &txp
+}
+
+func (tp *TxPoolImpl) Add(tx Tx) error {
+	tp.txMap[common.Base58Encode(tx.Hash())] = tx
+	return nil
+}
+
+func (tp *TxPoolImpl) Del(tx Tx) error {
+	delete(tp.txMap, common.Base58Encode(tx.Hash()))
+	return nil
+}
+
+func (tp *TxPoolImpl) Find(txHash []byte) (Tx, error) {
+	tx, ok := tp.txMap[common.Base58Encode(txHash)]
+	if !ok {
+		return tx, fmt.Errorf("not found")
+	}
+	return tx, nil
+}
+
+func (tp *TxPoolImpl) Has(txHash []byte) (bool, error) {
+
+	_, ok := tp.txMap[common.Base58Encode(txHash)]
+	return ok, nil
+}
+
+func (tp *TxPoolImpl) GetSlice() ([]Tx, error) {
+	var txs []Tx
+	for _, v := range tp.txMap {
+		txs = append(txs, v)
+	}
+
+	return txs, nil
+}
+
 
