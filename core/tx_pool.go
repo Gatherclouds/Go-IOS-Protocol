41cff3404dc75221be07e80b448119a1881c2b9f
wangzhihaiSF <1599315665@qq.com>
2018-04-22 14:10:52 +0800

    update tx_pool.go

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -82,13 +82,24 @@ func (tp *TxPoolImpl) Encode() []byte {
 		tp.TxHash = append(tp.TxHash, common.Base58Decode(k))
 		tp.Txs = append(tp.Txs, v)
 	}
-	bytes, err := tp.Marshal(nil)
-	if err != nil {
-		panic(err)
-	}
 	tp.TxHash = [][]byte{}
 	tp.Txs = []Tx{}
 	return bytes
+}
+
+func (tp *TxPoolImpl) Decode(a []byte) error {
+	_, err := tp.Unmarshal(a)
+	if err != nil {
+		return err
+	}
+	for i, v := range tp.TxHash {
+		tp.txMap[common.Base58Encode(v)] = tp.Txs[i]
+	}
+	return nil
+}
+
+func (tp *TxPoolImpl) Hash() []byte {
+	return common.Sha256(tp.Encode())
 }
 
 
