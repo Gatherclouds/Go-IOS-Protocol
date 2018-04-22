f85c6a700192e4c5b94b51c99e6b2afe4d93866e
wangzhihaiSF <1599315665@qq.com>
2018-04-22 14:20:16 +0800

    promote tx_pool_test.go ability

--- core/tx_pool_test.go
+++ core/tx_pool_test.go
@@ -3,7 +3,6 @@ package core
 import (
 	"testing"
 	"time"
-
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -18,6 +17,19 @@ func TestTxPoolImpl(t *testing.T) {
 			txp.Add(tx)
 			So(len(txp.txMap), ShouldEqual, 1)
 		})
-		
+		Convey("Del", func() {
+			txp.Del(tx)
+			So(len(txp.txMap), ShouldEqual, 0)
+		})
+
+		Convey("Find", func() {
+			txp.Add(tx)
+			tx2, err := txp.Find(tx.Hash())
+			So(err, ShouldBeNil)
+			So(tx2.Time, ShouldEqual, tx.Time)
+
+			_, err = txp.Find([]byte("hello"))
+			So(err, ShouldNotBeNil)
+		})
 	})
 }
