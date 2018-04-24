13f36f18d98ce1c28eb897d4321192c8a27d9f78
jrjtlite <terry.yannan@gmail.comgi>
2018-04-24 11:07:56 +0800

    modify binary_imp.go

--- iostdb/binary_imp.go
+++ iostdb/binary_imp.go
@@ -81,15 +81,4 @@ func (bin Binary) Hash160() Binary {
 	return bin
 }
 
-func Equal(a []byte, b []byte) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
-}
 
