d3f4420ff0ff1685b885efa590e5a167d15a84c2
wangzhihaiSF <1599315665@qq.comm>
2018-04-25 10:32:00 +0800

    perfect memdatabase.go

--- iostdb/memdatabase.go
+++ iostdb/memdatabase.go
@@ -20,3 +20,13 @@ func NewMemDatabase() (*MemDatabase, error) {
 	return &MemDatabase{db: make(map[string][]byte)}, nil
 }
 
+func NewMemDatabaseWithCap(size int) (*MemDatabase, error) {
+	return &MemDatabase{db: make(map[string][]byte, size)}, nil
+}
+
+func (db *MemDatabase) Put(key []byte, value []byte) error {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+	db.db[string(key)] = CopyBytes(value)
+	return nil
+}
