71405e92def8d5d2101cada35f7f2484f4aa8a61
He Peijan <glisxx6@gmail.com>
2018-04-26 10:46:23 +0800

    begin to write function

--- log/log.go
+++ log/log.go
@@ -3,6 +3,8 @@ package log
 import (
 	"os"
 	"sync"
+	"sync/atomic"
+	"runtime"
 )
 
 type Logger struct {
@@ -15,4 +17,28 @@ var instance *os.File
 var initialized int32
 var mu sync.Mutex
 
-const Path = "test.log"
+const Path = "test.log" // TODO : 在命令行内修改
+
+func NewLogger(tag string) (*Logger, error) {
+
+	if atomic.LoadInt32(&initialized) == 1 {
+		return &Logger{
+			logFile: instance,
+		}, nil
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+
+
+	runtime.SetFinalizer(instance, func(obj *os.File) {
+		obj.Close()
+	})
+
+	atomic.StoreInt32(&initialized, 1)
+
+	return &Logger{
+		logFile: instance,
+	}, nil
+
+}
