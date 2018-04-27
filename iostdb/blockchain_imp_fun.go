f929bea6be53da715da6ccb6909d469cb4a9c60d
tanky <itstangky@gmail.com>
2018-04-27 11:05:53 +0800

    fix some bugs

--- iostdb/blockchain_imp_fun.go
+++ iostdb/blockchain_imp_fun.go
@@ -7,16 +7,7 @@ func (bc *BlockChainImpl) Get(layer int) (*Block, error) {
 	if layer < 0 || layer >= bc.length {
 		return nil, fmt.Errorf("index exceed")
 	}
-
-	headHash, err := redis.Bytes(bc.redis.Do("LINDEX", IndexKey, layer))
-	if err != nil {
-		return nil, err
-	}
-
-	blk, err := bc.db.Get(headHash, &opt.ReadOptions{})
-	if err != nil {
-		return nil, err
-	}
+	
 	var block Block
 	block.Decode(blk)
 	return &block, nil
