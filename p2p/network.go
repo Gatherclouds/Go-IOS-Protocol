e119bc970604e9464c14a7957c5fd1d45c3f22e4
bitree8 <bitree8@gmail.com>
2018-04-27 10:20:27 +0800

    supply interface to network.go

--- p2p/network.go
+++ p2p/network.go
@@ -1,5 +1,7 @@
 package p2p
 
+import "net"
+
 type RequestHead struct {
 	Length uint32 // Request的长度信息
 }
@@ -11,4 +13,17 @@ type Response struct {
 	To          string
 	Code        int // http-like的状态码和描述
 	Description string
+}
+
+// 最基本网络的模块API，之后gossip协议，虚拟的网络延迟都可以在模块内部实现
+type Network interface {
+	Send(req Request)
+	Listen(port uint16) (<-chan Request, error)
+	Close(port uint16) error
+}
+
+type NaiveNetwork struct {
+	peerList *iostdb.LDBDatabase
+	listen   net.Listener
+	done     bool
 }
