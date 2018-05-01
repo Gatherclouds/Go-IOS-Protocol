6079077b8fc37a883fe5fec21f567a12b5e97c13
wangzhihaiSF <1599315665@qq.com>
2018-05-01 17:23:53 +0800

    add a vendor

--- p2p/peer_error.go
+++ p2p/peer_error.go
@@ -18,3 +18,18 @@ const (
 	DiscSubprotocolError = 0x10
 )
 
+var discReasonToString = [...]string{
+	DiscRequested:           "disconnect requested",
+	DiscNetworkError:        "network error",
+	DiscProtocolError:       "breach of protocol",
+	DiscUselessPeer:         "useless peer",
+	DiscTooManyPeers:        "too many peers",
+	DiscAlreadyConnected:    "already connected",
+	DiscIncompatibleVersion: "incompatible p2p protocol version",
+	DiscInvalidIdentity:     "invalid node identity",
+	DiscQuitting:            "client quitting",
+	DiscUnexpectedIdentity:  "unexpected identity",
+	DiscSelf:                "connected to self",
+	DiscReadTimeout:         "read timeout",
+	DiscSubprotocolError:    "subprotocol error",
+}
