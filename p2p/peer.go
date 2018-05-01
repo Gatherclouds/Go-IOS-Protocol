5c312bccd5051210de72f19fa6e72194083d1758
tanky <itstangky@gmail.com>
2018-05-01 17:11:18 +0800

    modify peer.go

--- p2p/peer.go
+++ p2p/peer.go
@@ -26,3 +26,22 @@ type Peer struct {
 	events   *event.Feed     // events receives message send / receive events if set
 }
 
+const (
+	// devp2p message codes
+	handshakeMsg = 0x00
+	discMsg      = 0x01
+	pingMsg      = 0x02
+	pongMsg      = 0x03
+	getPeersMsg  = 0x04
+	peersMsg     = 0x05
+)
+
+type protoRW struct {
+	Protocol
+	in     chan Msg        // receives read messages
+	closed <-chan struct{} // receives when peer is shutting down
+	wstart <-chan struct{} // receives when write may start
+	werr   chan<- error    // for write results
+	offset uint64
+	w      MsgWriter
+}
