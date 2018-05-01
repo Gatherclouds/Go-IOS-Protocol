c44d2df78ac229ab1788b81f27e06c68c564ef5b
He Peijian <glisxx6@gmail.com>
2018-05-01 17:38:44 +0800

    modified imports

--- p2p/protocol.go
+++ p2p/protocol.go
@@ -1,5 +1,7 @@
 package p2p
 
+import "fmt"
+
 // Protocol represents a P2P subprotocol implementation.
 type Protocol struct {
 	// Name should contain the official protocol name,
@@ -40,4 +42,20 @@ func (p Protocol) cap() Cap {
 type Cap struct {
 	Name    string
 	Version uint
+}
+
+func (cap Cap) RlpData() interface{} {
+	return []interface{}{cap.Name, cap.Version}
+}
+
+func (cap Cap) String() string {
+	return fmt.Sprintf("%s/%d", cap.Name, cap.Version)
+}
+
+type capsByNameAndVersion []Cap
+
+func (cs capsByNameAndVersion) Len() int      { return len(cs) }
+func (cs capsByNameAndVersion) Swap(i, j int) { cs[i], cs[j] = cs[j], cs[i] }
+func (cs capsByNameAndVersion) Less(i, j int) bool {
+	return cs[i].Name < cs[j].Name || (cs[i].Name == cs[j].Name && cs[i].Version < cs[j].Version)
 }
