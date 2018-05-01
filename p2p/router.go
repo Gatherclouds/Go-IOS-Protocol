bb7bd5cf803f787efc6a67d53f0d03ed61996b02
He Peijian <glisxx6@gmail.com>
2018-05-01 17:42:52 +0800

    add interface to router.go

--- p2p/router.go
+++ p2p/router.go
@@ -11,3 +11,17 @@ const (
 	ReqNewBlock
 )
 
+//go:generate mockgen -destination mocks/mock_router.go -package protocol_mock github.com/iost-official/PrototypeWorks/protocol Router
+
+/*
+Forwarding specific request to other components and sending messages for them
+*/
+type Router interface {
+	Init(base core.Network, port uint16) error
+	FilteredChan(filter Filter) (chan core.Request, error)
+	Run()
+	Stop()
+	Send(req core.Request)
+	Broadcast(req core.Request)
+	Download (req core.Request) chan []byte
+}
