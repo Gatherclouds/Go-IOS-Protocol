29586f5100ffa95d418b6754215526f0b46fa3d5
He Peijian <glisxx6@gmail.com>
2018-05-02 10:30:08 +0800

    create server.go

---
+++ p2p/server.go
@@ -0,0 +1,17 @@
+package p2p
+
+import "net"
+
+type connFlag int
+
+// Conn represents a connection between two nodes in the network
+type conn struct {
+	fd net.Conn
+	transport
+	flags connFlag
+	cont  chan error      // The run loop uses cont to signal errors to SetupConn.
+	id    discover.NodeID // valid after the encryption handshake
+	caps  []Cap           // valid after the protocol handshake
+	name  string          // valid after the protocol handshake
+}
+
