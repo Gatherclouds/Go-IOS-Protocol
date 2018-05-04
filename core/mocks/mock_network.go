135197d2ed5e1c38c8dd2f6df21aa6e5d80cca78
He Peijian <glisxx6@gmail>
2018-05-04 10:53:15 +0800

    added NewMockNetwork to create a new mock instance

--- core/mocks/mock_network.go
+++ core/mocks/mock_network.go
@@ -19,3 +19,9 @@ type MockNetworkMockRecorder struct {
 	mock *MockNetwork
 }
 
+// NewMockNetwork creates a new mock instance
+func NewMockNetwork(ctrl *gomock.Controller) *MockNetwork {
+	mock := &MockNetwork{ctrl: ctrl}
+	mock.recorder = &MockNetworkMockRecorder{mock}
+	return mock
+}
