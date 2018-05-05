e90fbabda6ed4417390866a5e63b7512e4c966af
He Peijian <glisxx6@gmail.comm>
2018-05-05 10:53:13 +0800

    modified struts_schema_gen.go

--- common/struts_schema_gen.go
+++ common/struts_schema_gen.go
@@ -17,3 +17,39 @@ type SignatureRaw struct {
 	Sig       []byte
 	Pubkey    []byte
 }
+
+func (d *SignatureRaw) Size() (s uint64) {
+
+	{
+		l := uint64(len(d.Sig))
+
+		{
+
+			t := l
+			for t >= 0x80 {
+				t >>= 7
+				s++
+			}
+			s++
+
+		}
+		s += l
+	}
+	{
+		l := uint64(len(d.Pubkey))
+
+		{
+
+			t := l
+			for t >= 0x80 {
+				t >>= 7
+				s++
+			}
+			s++
+
+		}
+		s += l
+	}
+	s += 1
+	return
+}
