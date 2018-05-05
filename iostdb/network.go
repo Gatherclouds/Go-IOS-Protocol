ccaef8637696a3f0e33c33c5024b179317bf0b66
wangzhihaiSF <1599315665@qq.com>
2018-05-05 10:17:48 +0800

    modified network

--- iostdb/network.go
+++ iostdb/network.go
@@ -16,10 +16,3 @@ type Response struct {
 	Description string
 }
 
-//go:generate mockgen -destination mocks/mock_network.go -package iosbase_mock -source network.go -imports .=github.com/iost-official/Go-IOS-Protocol/iosbase
-
-type Net interface {
-	Send(req Request) chan Response
-	Listen(port uint16) (chan Request, chan Response, error)
-	Close(port uint16) error
-}
