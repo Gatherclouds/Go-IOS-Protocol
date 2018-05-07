b85fa12041f121117855fb62f781f766cd6b26b7
wangzhihaiSF <1599315665@qq.com>
2018-05-07 10:32:43 +0800

    consummate console.go

--- console/console.go
+++ console/console.go
@@ -1,6 +1,12 @@
 package console
 
-import "sync"
+import (
+	"sync"
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
 
 type Console struct {
 	cmds    []Cmd
@@ -24,3 +30,18 @@ func (c *Console) Init(cmds ...Cmd) error {
 func (c *Console) RegisterCmd(cmd Cmd) {
 	c.cmds = append(c.cmds, cmd)
 }
+
+func (c *Console) Listen(prompt string) {
+	for c.running {
+		var cmd string
+		fmt.Print(prompt)
+		reader := bufio.NewReader(os.Stdin)
+		cmd, _ = reader.ReadString('\n')
+		args := strings.Fields(cmd)
+		if len(args) == 0 {
+			continue
+		}
+		fmt.Print(c.Run(args[0], args[1:]))
+	}
+}
+
