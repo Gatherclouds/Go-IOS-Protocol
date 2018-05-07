c965c684106972697eedbc2624b88463170b72d7
wangzhihaiSF <1599315665@qq.com>
2018-05-07 10:28:38 +0800

    added functions

--- console/console.go
+++ console/console.go
@@ -11,3 +11,16 @@ var Wg sync.WaitGroup
 var Done = make(chan struct{})
 var Nn *p2p.NaiveNetwork
 var Db *iostdb.LDBDatabase
+
+func (c *Console) Init(cmds ...Cmd) error {
+	c.cmds = make([]Cmd, 0)
+	c.running = true
+	for _, cc := range cmds {
+		c.RegisterCmd(cc)
+	}
+	return nil
+}
+
+func (c *Console) RegisterCmd(cmd Cmd) {
+	c.cmds = append(c.cmds, cmd)
+}
