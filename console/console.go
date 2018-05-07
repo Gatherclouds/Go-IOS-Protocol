ee3b460b27d90507faaeee9be9f9779def990c4f
wangzhihaiSF <1599315665@qq.com>
2018-05-07 10:33:44 +0800

    added a funtion

--- console/console.go
+++ console/console.go
@@ -45,3 +45,15 @@ func (c *Console) Listen(prompt string) {
 	}
 }
 
+func (c *Console) Run(name string, args []string) string {
+	name = strings.ToLower(name)
+
+	for _, cmd := range c.cmds {
+		if cmd.Name != name {
+			continue
+		}
+		return cmd.Exec(c, args)
+	}
+	return "Command not found\n"
+}
+
