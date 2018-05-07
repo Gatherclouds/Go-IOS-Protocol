2dccfa4c0f90951e24d848904bc21afac731c559
tanky <itstangky@gmail.com>
2018-05-07 11:02:38 +0800

    added printchain.go

---
+++ console/printchain.go
@@ -0,0 +1,37 @@
+package console
+
+import "fmt"
+
+func Printchain() Cmd {
+	return Cmd{
+		Name:  "printchain",
+		Usage: `printchain - Print all the blocks of the blockchain`,
+		Exec: func(host *Console, args []string) string {
+			bc, to_print := transaction.NewBlockchain("", Db)
+
+			if bc == nil {
+				return to_print
+			}
+
+			//defer bc.Db.Close()
+
+			bci := bc.Iterator()
+
+			for {
+				block := bci.Next()
+
+				to_print += fmt.Sprintf("Prev hash: %x\n", block.PrevBlockHash)
+				to_print += fmt.Sprintf("Hash: %x\n", block.Hash)
+				for _, tx := range(block.Transactions) {
+					to_print += tx.String()
+				}
+				to_print += "\n"
+
+				if len(block.PrevBlockHash) == 0 {
+					break
+				}
+			}
+			return to_print
+		},
+	}
+}
