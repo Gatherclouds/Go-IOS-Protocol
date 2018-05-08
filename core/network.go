3b05ce740b9835d77df95eb0417bd5c02ffdd29d
tanky <itstangky@gmail.com>
2018-05-08 10:30:20 +0800

    removed redundant codes

--- core/network.go
+++ core/network.go
@@ -8,14 +8,6 @@ type Request struct {
 	Body    []byte
 }
 
-//type Response struct {
-//	Time        int64
-//	From        string
-//	To          string
-//	Code        int
-//	Description string
-//}
-
 //go:generate mockgen -destination mocks/mock_network.go -package iosbase_mock -source network.go -imports .=github.com/iost-official/PrototypeWorks/iosbase
 
 type Network interface {
