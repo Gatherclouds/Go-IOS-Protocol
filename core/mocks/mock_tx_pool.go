792bff1e4ddd7280d363a56a158dab5a7fe34f55
wangzhihaiSF <1599315665@qq.com>
2018-05-09 11:05:08 +0800

    Find mocks base method

--- core/mocks/mock_tx_pool.go
+++ core/mocks/mock_tx_pool.go
@@ -78,3 +78,11 @@ func (m *MockTxPool) Encode() []byte {
 func (mr *MockTxPoolMockRecorder) Encode() *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Encode", reflect.TypeOf((*MockTxPool)(nil).Encode))
 }
+
+// Find mocks base method
+func (m *MockTxPool) Find(arg0 []byte) (core.Tx, error) {
+	ret := m.ctrl.Call(m, "Find", arg0)
+	ret0, _ := ret[0].(core.Tx)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
