4bc8857aacb5eb95de05f586d915c2a6f110aaa3
bitree8 <bitree8@gmail.com>
2018-05-11 11:08:59 +0800

    added fucntions

--- pow/cached_blockchain.go
+++ pow/cached_blockchain.go
@@ -1,6 +1,9 @@
 package pow
 
-import "github.com/ethereum/go-ethereum/core"
+import (
+	"github.com/ethereum/go-ethereum/core"
+	"fmt"
+)
 
 type CachedBlockChain struct {
 	core.BlockChain
@@ -14,3 +17,19 @@ func NewCBC(chain core.BlockChain) CachedBlockChain {
 	}
 }
 
+func (c *CachedBlockChain) Get(layer int) (*core.Block, error) {
+	if layer < 0 || layer >= c.BlockChain.Length()+len(c.cachedBlock) {
+		return nil, fmt.Errorf("overflow")
+	}
+	if layer < c.BlockChain.Length() {
+		return c.BlockChain.Get(layer)
+	}
+	return c.cachedBlock[layer-c.BlockChain.Length()], nil
+}
+func (c *CachedBlockChain) Push(block *core.Block) error {
+	c.cachedBlock = append(c.cachedBlock, block)
+	return nil
+}
+func (c *CachedBlockChain) Length() int {
+	return c.BlockChain.Length() + len(c.cachedBlock)
+}
