1109b941802b69146160a532d1fbfba0943ddf84
bitree8 <bitree8@gmail.com>
2018-05-15 10:33:52 +0800

    added function

--- account/info/user_balance.go
+++ account/info/user_balance.go
@@ -1,5 +1,10 @@
 package info
 
+import (
+	"strconv"
+	"encoding/base32"
+)
+
 type UserBalance struct {
 	UserInfo
 }
@@ -10,3 +15,9 @@ const (
 	UserBalance_Encoding_Head_Length = len(UserBalance_Encoding_Head)
 	UserBalance_Encoding_Tail_Length = len(UserBalance_Encoding_Tail)
 )
+
+func (balance *UserBalance) Encode(val uint64) string {
+	raw := ([]byte)(UserBalance_Encoding_Head + strconv.FormatUint(val, 10) + UserBalance_Encoding_Tail)
+	return base32.HexEncoding.EncodeToString(raw) //or other encoding forms
+}
+
