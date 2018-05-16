bd1e161060fd19eb6fb1d40e70d0e7408340b2b5
zrp <zrpherule@gmail.com>
2018-05-16 11:12:41 +0800

    added NewDPos to  creates DPos instance

--- iostdb/mock_statepool_fun.go
+++ iostdb/mock_statepool_fun.go
@@ -1,7 +1,6 @@
 package iostdb
 
 import (
-	"reflect"
 	"github.com/golang/mock/gomock"
 )
 
@@ -17,10 +16,52 @@ func (m *MockStatePool) EXPECT() *MockStatePoolMockRecorder {
 	return m.recorder
 }
 
-// Init indicates an expected call of Init
-func (mr *MockStatePoolMockRecorder) Init() *gomock.Call {
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Init", reflect.TypeOf((*MockStatePool)(nil).Init))
+// NewDPoS: 新建一个DPoS实例
+// acc: 节点的Coinbase账户, bc: 基础链(从数据库读取), witnessList: 见证节点列表
+func NewDPoS(acc Account, bc block.Chain, witnessList []string /*, network core.Network*/) (*DPoS, error) {
+	p := DPoS{}
+	p.Account = acc
+	p.blockCache = NewBlockCache(bc, len(witnessList)*2/3+1)
+
+	var err error
+	p.router, err = RouterFactory("base")
+	if err != nil {
+		return nil, err
+	}
+
+	p.synchronizer = NewSynchronizer(p.blockCache, p.router)
+	if p.synchronizer == nil {
+		return nil, err
+	}
+
+	//	Tx chan init
+	p.chTx, err = p.router.FilteredChan(Filter{
+		WhiteList:  []message.Message{},
+		BlackList:  []message.Message{},
+		RejectType: []ReqType{},
+		AcceptType: []ReqType{
+			ReqPublishTx,
+			reqTypeVoteTest, // Only for test
+		}})
+	if err != nil {
+		return nil, err
+	}
+
+	//	Block chan init
+	p.chBlock, err = p.router.FilteredChan(Filter{
+		WhiteList:  []message.Message{},
+		BlackList:  []message.Message{},
+		RejectType: []ReqType{},
+		AcceptType: []ReqType{ReqNewBlock}})
+	if err != nil {
+		return nil, err
+	}
+
+	p.initGlobalProperty(p.Account, witnessList)
+	return &p, nil
 }
+
+
 
 
 
