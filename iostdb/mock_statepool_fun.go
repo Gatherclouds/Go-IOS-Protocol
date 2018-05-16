eacf8d5565ca81e35261611b2f5e96b09e86e0dc
zrp <zrpherule@gmail.com>
2018-05-16 11:17:38 +0800

    updated dpos.go

--- iostdb/mock_statepool_fun.go
+++ iostdb/mock_statepool_fun.go
@@ -16,55 +16,6 @@ func (m *MockStatePool) EXPECT() *MockStatePoolMockRecorder {
 	return m.recorder
 }
 
-// NewDPoS: 新建一个DPoS实例
-// acc: 节点的Coinbase账户, bc: 基础链(从数据库读取), witnessList: 见证节点列表
-func NewDPoS(acc Account, bc block.Chain, witnessList []string /*, network core.Network*/) (*DPoS, error) {
-	p := DPoS{}
-	p.Account = acc
-	p.blockCache = NewBlockCache(bc, len(witnessList)*2/3+1)
-
-	var err error
-	p.router, err = RouterFactory("base")
-	if err != nil {
-		return nil, err
-	}
-
-	p.synchronizer = NewSynchronizer(p.blockCache, p.router)
-	if p.synchronizer == nil {
-		return nil, err
-	}
-
-	//	Tx chan init
-	p.chTx, err = p.router.FilteredChan(Filter{
-		WhiteList:  []message.Message{},
-		BlackList:  []message.Message{},
-		RejectType: []ReqType{},
-		AcceptType: []ReqType{
-			ReqPublishTx,
-			reqTypeVoteTest, // Only for test
-		}})
-	if err != nil {
-		return nil, err
-	}
-
-	//	Block chan init
-	p.chBlock, err = p.router.FilteredChan(Filter{
-		WhiteList:  []message.Message{},
-		BlackList:  []message.Message{},
-		RejectType: []ReqType{},
-		AcceptType: []ReqType{ReqNewBlock}})
-	if err != nil {
-		return nil, err
-	}
-
-	p.initGlobalProperty(p.Account, witnessList)
-	return &p, nil
-}
-
-func (p *DPoS) initGlobalProperty(acc Account, witnessList []string) {
-	p.globalStaticProperty = newGlobalStaticProperty(acc, witnessList)
-	p.globalDynamicProperty = newGlobalDynamicProperty()
-}
 
 
 
