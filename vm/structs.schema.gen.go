47792abddc6d314276887f4dbf329e77767abb63
jrjtlite <terry.yannan@gmail.comgi>
2018-05-19 10:37:55 +0800

    modifying of Marshal

--- vm/structs.schema.gen.go
+++ vm/structs.schema.gen.go
@@ -75,6 +75,25 @@ func (d *contractInfoRaw) Marshal(buf []byte) ([]byte, error) {
 		buf[i+0+0] = byte(d.Version >> 0)
 
 	}
+	{
+
+		buf[i+0+1] = byte(d.GasLimit >> 0)
+
+		buf[i+1+1] = byte(d.GasLimit >> 8)
+
+		buf[i+2+1] = byte(d.GasLimit >> 16)
+
+		buf[i+3+1] = byte(d.GasLimit >> 24)
+
+		buf[i+4+1] = byte(d.GasLimit >> 32)
+
+		buf[i+5+1] = byte(d.GasLimit >> 40)
+
+		buf[i+6+1] = byte(d.GasLimit >> 48)
+
+		buf[i+7+1] = byte(d.GasLimit >> 56)
+
+	}
 
 	return buf[:i+17], nil
 }
