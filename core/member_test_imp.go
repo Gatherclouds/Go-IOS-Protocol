688e9059db9609bef3ff2676e0b3d9c5d7247f34
zrp <zrpherule@gmail@.com>
2018-05-21 11:17:11 +0800

    MockContractMockRecorder is the mock recorder for MockContract

--- core/member_test_imp.go
+++ core/member_test_imp.go
@@ -5,15 +5,5 @@ type TxPoolImpl struct {
 	txMap map[string]Tx
 }
 
-func NewTxPool() TxPool {
-	txp := TxPoolImpl{
-		txMap: make(map[string]Tx),
-		TxPoolRaw: TxPoolRaw{
-			Txs:    make([]Tx, 0),
-			TxHash: make([][]byte, 0),
-		},
-	}
-	return &txp
-}
 
 
--- vm/mocks/mock_contract.go
+++ vm/mocks/mock_contract.go
@@ -10,3 +10,8 @@ type MockContract struct {
 	ctrl     *gomock.Controller
 	recorder *MockContractMockRecorder
 }
+
+// MockContractMockRecorder is the mock recorder for MockContract
+type MockContractMockRecorder struct {
+	mock *MockContract
+}
