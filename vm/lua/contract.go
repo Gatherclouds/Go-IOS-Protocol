91620b53c018791c193aa1e5ac86b598bef661b3
He Peijian <glisxx6@gmail.com>
2018-05-21 10:40:19 +0800

    finished contract.go

--- vm/lua/contract.go
+++ vm/lua/contract.go
@@ -61,3 +61,16 @@ func (c *Contract) Decode(b []byte) error {
 func (c *Contract) Hash() []byte {
 	return common.Sha256(c.Encode())
 }
+
+func NewContract(info vm.ContractInfo, code string, main Method, apis ...Method) Contract {
+	c := Contract{
+		info: info,
+		code: code,
+		main: main,
+	}
+	c.apis = make(map[string]Method)
+	for _, api := range apis {
+		c.apis[api.name] = api
+	}
+	return c
+}
