c6b75e17bbe6f59d43bdfaec46928ae8cdb46567
zrp <zrpherule@gmail@.comm>
2018-05-22 11:14:33 +0800

    API indicates an expected call of API

--- vm/mocks/mock_contract.go
+++ vm/mocks/mock_contract.go
@@ -3,6 +3,7 @@ package mocks
 import (
 	gomock "github.com/golang/mock/gomock"
 
+	"reflect"
 )
 
 // MockContract is a mock of Contract interface
@@ -34,6 +35,11 @@ func (m *MockContract) API(arg0 string) (vm.Method, error) {
 	ret0, _ := ret[0].(vm.Method)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
+}
+
+// API indicates an expected call of API
+func (mr *MockContractMockRecorder) API(arg0 interface{}) *gomock.Call {
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "API", reflect.TypeOf((*MockContract)(nil).API), arg0)
 }
 
 // AddSigner mocks base method
