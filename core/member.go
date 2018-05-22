a74f17519d2a7127bca1ca7d77b6bae72a9c2448
tanky <itstangky@gmail.com>
2018-05-22 11:22:00 +0800

    method deleting of member.go in core

--- core/member.go
+++ core/member.go
@@ -1,30 +1,12 @@
 package core
 
-import (
-	"bytes"
-	"encoding/binary"
-	"fmt"
-	"math/rand"
-	"time"
-)
-
 type Member struct {
 	ID     string
 	Pubkey []byte
 	Seckey []byte
 }
 
-func NewMember(seckey []byte) (Member, error) {
-	var m Member
-	if seckey == nil {
-		seckey = randomSeckey()
-	}
 
-	m.Seckey = seckey
-	m.Pubkey = makePubkey(seckey)
-	m.ID = GetIdByPubkey(m.Pubkey)
-	return m, nil
-}
 
 
 
