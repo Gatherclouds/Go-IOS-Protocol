e0e44c35798ad9a15f711f146ac1e03a093ed4a1
tanky <itstangky@gmail.com>
2018-05-23 12:35:54 +0800

    update of struts.schema.gen.go

--- verifier/struts.schema.gen.go
+++ verifier/struts.schema.gen.go
@@ -55,3 +55,35 @@ func (d *VerifyLogRaw) Size() (s uint64) {
 	}
 	return
 }
+
+func (d *VerifyLogRaw) Marshal(buf []byte) ([]byte, error) {
+	size := d.Size()
+	{
+		if uint64(cap(buf)) >= size {
+			buf = buf[:size]
+		} else {
+			buf = make([]byte, size)
+		}
+	}
+	i := uint64(0)
+
+	{
+		l := uint64(len(d.List))
+
+		{
+
+			t := uint64(l)
+
+			for t >= 0x80 {
+				buf[i+0] = byte(t) | 0x80
+				t >>= 7
+				i++
+			}
+			buf[i+0] = byte(t)
+			i++
+
+		}
+		
+	}
+	return buf[:i+0], nil
+}
