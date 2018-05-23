c8070c8e73940f2acf08a78db414dfade505d168
bitree8 <1599315665@qq.com>
2018-05-23 12:17:27 +0800

    modified the file

--- verifier/struts.schema.gen.go
+++ verifier/struts.schema.gen.go
@@ -15,3 +15,23 @@ var (
 type VerifyLogRaw struct {
 	List [][]int32
 }
+
+func (d *VerifyLogRaw) Size() (s uint64) {
+
+	{
+		l := uint64(len(d.List))
+
+		{
+
+			t := l
+			for t >= 0x80 {
+				t >>= 7
+				s++
+			}
+			s++
+
+		}
+
+	}
+	return
+}
