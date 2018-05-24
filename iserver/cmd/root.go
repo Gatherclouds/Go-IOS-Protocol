1b38f122582c5d44daffc5cf014edbd506d229e4
tanky <itstangky@gmail.com>
2018-05-24 09:01:39 +0800

    increase of database path

--- iserver/cmd/root.go
+++ iserver/cmd/root.go
@@ -10,3 +10,27 @@ type ServerExit interface {
 
 var serverExit []ServerExit
 
+
+func exitLoop() {
+	exit := make(chan bool)
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
+	defer signal.Stop(c)
+	defer close(exit)
+
+	go func() {
+		<-c
+		fmt.Printf("IOST server received interrupt, shutting down...")
+
+		for _, s := range serverExit {
+			if s != nil {
+				s.Stop()
+			}
+		}
+
+		os.Exit(0)
+	}()
+
+	<-exit
+}
