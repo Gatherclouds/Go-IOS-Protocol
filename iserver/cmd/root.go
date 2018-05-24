d2a2effd98915454ac31d98ff34a97844ac78a20
tanky <itstangky@gmail.com>
2018-05-24 09:07:38 +0800

    improvement of execute performance

--- iserver/cmd/root.go
+++ iserver/cmd/root.go
@@ -1,5 +1,14 @@
 package cmd
 
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
 var cfgFile string
 var logFile string
 var dbFile string
@@ -34,3 +43,22 @@ func exitLoop() {
 
 	<-exit
 }
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.iserver.yaml)")
+	rootCmd.PersistentFlags().StringVar(&logFile, "log", "", "log file (default is ./iserver.log)")
+	rootCmd.PersistentFlags().StringVar(&dbFile, "db", "", "database file (default is ./data.db)")
+	viper.BindPFlag("config", rootCmd.PersistentFlags().Lookup("config"))
+	viper.BindPFlag("log", rootCmd.PersistentFlags().Lookup("log"))
+	viper.BindPFlag("db", rootCmd.PersistentFlags().Lookup("db"))
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
