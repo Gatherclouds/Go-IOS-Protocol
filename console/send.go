423595e0dd6077a994b56a20025905fea69b503b
wangzhihaiSF <1599315665@qq.com>
2018-05-24 08:42:55 +0800

    modify send.go

--- console/send.go
+++ console/send.go
@@ -1,6 +1,5 @@
 package console
 
-import "strconv"
 
 func Send() Cmd {
 	return Cmd{
@@ -10,29 +9,10 @@ func Send() Cmd {
 			if len(args) != 3 {
 				return "Invalid arguments!\n"
 			}
-			from := args[0]
-			to := args[1]
-			amount, err := strconv.Atoi(args[2])
+
 			if err != nil {
 				return "Invalid arguments!\n"
 			}
-
-			bc, to_print := transaction.NewBlockchain(from, Db)
-
-			if bc == nil {
-				return to_print
-			}
-
-			//defer bc.Db.Close()
-
-			tx, to_print := transaction.NewUTXOTransaction(from, to, amount, bc)
-
-			if tx == nil {
-				return to_print
-			}
-
-			bc.MineBlock([]*transaction.Transaction{tx}, Nn)
-			to_print += "\nSuccess!\n"
 			return to_print
 		},
 	}
