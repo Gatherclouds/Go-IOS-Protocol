e4dc154d18a7b7b02a0203a285b131dd77602e1c
bitree8 <bitree8@gamil.com>
2018-05-28 10:59:06 +0800

    TestCacheVerifier method is added

--- verifier/verifier_test.go
+++ verifier/verifier_test.go
@@ -38,3 +38,43 @@ func TestGenesisVerify(t *testing.T) {
 	})
 }
 
+
+func TestCacheVerifier(t *testing.T) {
+	Convey("Test of CacheVerifier", t, func() {
+		Convey("Verify contract", func() {
+			mockCtl := gomock.NewController(t)
+			pool := core_mock.NewMockPool(mockCtl)
+
+			var k state.Key
+			var v state.Value
+
+			pool.EXPECT().Put(gomock.Any(), gomock.Any()).AnyTimes().Do(func(key state.Key, value state.Value) error {
+				k = key
+				v = value
+				return nil
+			})
+
+			pool.EXPECT().Get(gomock.Any()).AnyTimes().Return(state.MakeVFloat(3.14), nil)
+
+			var k2 state.Key
+			var f2 state.Key
+			var v2 state.Value
+			pool.EXPECT().PutHM(gomock.Any(), gomock.Any(), gomock.Any()).Do(func(key, field state.Key, value state.Value) {
+				k2 = key
+				f2 = field
+				v2 = value
+			})
+			v3 := state.MakeVFloat(float64(10000))
+			pool.EXPECT().GetHM(gomock.Any(), gomock.Any()).Return(v3, nil)
+			pool.EXPECT().Copy().AnyTimes().Return(pool)
+			main := lua.NewMethod("main", 0, 1)
+			code := `function main()
+	a = Get("pi")
+	Put("hello", a)
+	return "success"
+end`
+
+		})
+	})
+}
+
