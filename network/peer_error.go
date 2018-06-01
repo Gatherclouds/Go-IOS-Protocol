15df85277bcd8ad49e8d7f5e33a2eac908990c64
He Peijian <glisxx6@gmail.com>
2018-06-01 11:05:01 +0800

    finish peer_error.go

--- network/peer_error.go
+++ network/peer_error.go
@@ -17,5 +17,19 @@ const (
 	DiscReadTimeout
 	DiscSubprotocolError = 0x10
 )
-
+var discReasonToString = [...]string{
+	DiscRequested:           "disconnect requested",
+	DiscNetworkError:        "network error",
+	DiscProtocolError:       "breach of protocol",
+	DiscUselessPeer:         "useless peer",
+	DiscTooManyPeers:        "too many peers",
+	DiscAlreadyConnected:    "already connected",
+	DiscIncompatibleVersion: "incompatible network protocol version",
+	DiscInvalidIdentity:     "invalid node identity",
+	DiscQuitting:            "client quitting",
+	DiscUnexpectedIdentity:  "unexpected identity",
+	DiscSelf:                "connected to self",
+	DiscReadTimeout:         "read timeout",
+	DiscSubprotocolError:    "subprotocol error",
+}
 
