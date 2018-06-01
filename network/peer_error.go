cc3a2d26fa7a10d1af6d1c9cf8d0856a493b8b3b
jrjtlite <terry.yannan@gmail.comgi>
2018-06-01 11:02:34 +0800

    creation of peer_error.go

---
+++ network/peer_error.go
@@ -0,0 +1,21 @@
+package network
+
+type DiscReason uint
+
+const (
+	DiscRequested DiscReason = iota
+	DiscNetworkError
+	DiscProtocolError
+	DiscUselessPeer
+	DiscTooManyPeers
+	DiscAlreadyConnected
+	DiscIncompatibleVersion
+	DiscInvalidIdentity
+	DiscQuitting
+	DiscUnexpectedIdentity
+	DiscSelf
+	DiscReadTimeout
+	DiscSubprotocolError = 0x10
+)
+
+
