35faafba1c057af12a4becb9a451c22f396d0351
Dominator008 <zhoumotongxue008@gmail.com>
2018-06-02 11:25:16 +0800

    testBootNodeConn method is added

--- network/main/main.go
+++ network/main/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"net"
+)
 
 var serverAddr = flag.String("s", "", "server port 30304, or other ports that have already started.")
 var conf = initNetConf()
@@ -17,5 +21,21 @@ func main() {
 	bootnodeStart()
 	//testBaseNetwork()
 	//testBootNodeConn()
+}
+
+func testBootNodeConn() {
+	node, err := discover.ParseNode("84a8ecbeeb6d3f676da1b261c35c7cd15ae17f32b659a6f5ce7be2d60f6c16f9@18.219.254.124:30304")
+
+	if err != nil {
+		fmt.Errorf("parse boot node got err:%v", err)
+	}
+	conn, err := net.Dial("tcp", node.Addr())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	conn.Write([]byte("111"))
+	conn.Close()
+	fmt.Println("conn to remote success!")
 }
 
