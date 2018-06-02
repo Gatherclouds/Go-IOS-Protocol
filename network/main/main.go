dc5d2b3f06e214cec610690808622e71bc2573f2
wangzhihaiSF <1599315665@qq.com>
2018-06-02 13:27:33 +0800

    improve the performance

--- network/main/main.go
+++ network/main/main.go
@@ -40,4 +40,25 @@ func testBootNodeConn() {
 	fmt.Println("conn to remote success!")
 }
 
-
+func bootnodeStart() {
+	node, err := discover.ParseNode("84a8ecbeeb6d3f676da1b261c35c7cd15ae17f32b659a6f5ce7be2d60f6c16f9@0.0.0.0:30304")
+	if err != nil {
+		fmt.Errorf("parse boot node got err:%v", err)
+	}
+	router, _ := RouterFactory("base")
+	conf := initNetConf()
+	conf.SetNodeID(string(node.ID))
+	baseNet, err := NewBaseNetwork(conf)
+	if err != nil {
+		fmt.Println("NewBaseNetwork ", err)
+		return
+	}
+	err = router.Init(baseNet, node.TCP)
+	if err != nil {
+		fmt.Println("Init ", err)
+		return
+	}
+	go router.Run()
+	fmt.Println("server starting", node.Addr())
+	select {}
+}
