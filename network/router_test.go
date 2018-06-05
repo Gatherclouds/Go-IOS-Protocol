7e57df2df1cdc6c35123ec611fc7485ee2b3472e
wangzhihaiSF <1599315665@qq.com>
2018-06-05 10:32:30 +0800

    update router_test.go

--- network/router_test.go
+++ network/router_test.go
@@ -4,6 +4,10 @@ import (
 	"testing"
 	. "github.com/smartystreets/goconvey/convey"
 
+	"io/ioutil"
+	"os"
+	"strconv"
+	"time"
 )
 
 func TestRouterImpl_Init(t *testing.T) {
@@ -15,3 +19,16 @@ func TestRouterImpl_Init(t *testing.T) {
 		So(router.(*RouterImpl).port, ShouldEqual, 30601)
 	})
 }
+
+func TestGetInstance(t *testing.T) {
+	Convey("", t, func() {
+
+		router, err := GetInstance(&NetConifg{NodeTablePath: "tale_test", ListenAddr: "127.0.0.1"}, "base", 30304)
+
+		So(err, ShouldBeNil)
+		So(router.(*RouterImpl).port, ShouldEqual, uint16(30304))
+		So(Route.(*RouterImpl).port, ShouldEqual, uint16(30304))
+	})
+}
+
+
