cbd141f63dc41fc3c20ff8d9174bc19abdde292f
Dominator008 <zhoumotongxue008@gmail.com>
2018-06-06 10:57:45 +0800

    annotation is attached

--- rpc/cli.pb.go
+++ rpc/cli.pb.go
@@ -1,9 +1,11 @@
 package rpc
 
 import proto "github.com/golang/protobuf/proto"
+import fmt "fmt"
+import math "math"
+
 import (
-    fmt "fmt"
-    math "math"
+	context "golang.org/x/net/context"
 	grpc "google.golang.org/grpc"
 )
 
@@ -11,6 +13,13 @@ import (
 var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the proto package it is being compiled against.
+// A compilation error at this line likely means your copy of the
+// proto package needs to be updated.
+const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
+
 
 func (m *Transaction) Reset()         { *m = Transaction{} }
 func (m *Transaction) String() string { return proto.CompactTextString(m) }
