b936899a456d08992eb2b03338220604cd999a0b
wangzhihaiSF <1599315665@qq.com>
2018-06-06 10:33:42 +0800

    create rpc/cli.pb.go

---
+++ rpc/cli.pb.go
@@ -0,0 +1,9 @@
+package rpc
+
+func (m *Transaction) Reset()         { *m = Transaction{} }
+func (m *Transaction) String() string { return proto.CompactTextString(m) }
+func (*Transaction) ProtoMessage()    {}
+func (*Transaction) Descriptor() ([]byte, []int) {
+	return fileDescriptor_cli_7f181ac8c7362e80, []int{0}
+}
+
