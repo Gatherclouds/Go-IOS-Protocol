91a57284ab08233e24e636420244ed844dd2b455
jrjtlite <terry.yannan@gmail.comgi>
2018-06-06 10:40:54 +0800

    Reference imports to suppress errors if they are not otherwise used

--- rpc/cli.pb.go
+++ rpc/cli.pb.go
@@ -1,5 +1,16 @@
 package rpc
 
+import proto "github.com/golang/protobuf/proto"
+import (
+	"fmt"
+	"math"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ = proto.Marshal
+var _ = fmt.Errorf
+var _ = math.Inf
+
 func (m *Transaction) Reset()         { *m = Transaction{} }
 func (m *Transaction) String() string { return proto.CompactTextString(m) }
 func (*Transaction) ProtoMessage()    {}
