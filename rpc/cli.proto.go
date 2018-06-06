76bb14bcb8f0c12a8261cf937e5141817b91f020
deBreakr <wang.yannan@outlook.com>
2018-06-06 11:27:43 +0800

    cli.proto.go has been finished

--- rpc/cli.proto.go
+++ rpc/cli.proto.go
@@ -18,3 +18,24 @@ bytes tx = 1;
 message Response {
 int32 code = 1;
 }
+
+message TransactionKey {
+string publisher = 1;
+int64 nonce = 2;
+}
+
+message Key {
+string s = 1;
+}
+
+message Value {
+string sv = 2;
+}
+
+message BlockKey {
+int64 layer = 1;
+}
+
+message BlockInfo {
+string json = 1;
+}
