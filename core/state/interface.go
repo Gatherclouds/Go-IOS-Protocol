67375163e33ed35d9d57e96e1718dd7838597d08
wangzhihaiSF <1599315665@qq.com>
2018-06-07 11:46:51 +0800

    consummate Pool storage pairs

--- core/state/interface.go
+++ core/state/interface.go
@@ -1,7 +1,16 @@
 package state
 
-type Serializable interface {
-	Encode() []byte
-	Decode([]byte) error
-	Hash() []byte
+// state pool存储对状态机进行修改的链，可以方便地记录所有的状态同时易于回退
+type Pool interface {
+	Copy() Pool
+	GetPatch() Patch
+	Flush() error
+
+	Put(key Key, value Value)
+	Get(key Key) (Value, error)
+	Has(key Key) bool
+	Delete(key Key)
+
+	GetHM(key, field Key) (Value, error)
+	PutHM(key, field Key, value Value) error
 }
