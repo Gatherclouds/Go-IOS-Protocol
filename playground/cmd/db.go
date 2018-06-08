58b76cc8a6d212f21e54148970acb00cede2a8c9
Dominator008 <zhoumotongxue008@gmail.com>
2018-06-08 11:09:10 +0800

    db.go is modified

--- playground/cmd/db.go
+++ playground/cmd/db.go
@@ -8,3 +8,13 @@ func (d *Database) Put(key []byte, value []byte) error {
 	d.Normal[string(key)] = value
 	return nil
 }
+
+func (d *Database) PutHM(key []byte, args ...[]byte) error {
+	key1 := string(key)
+	key2 := string(args[0])
+	d.Normal[key1+"."+key2] = args[1]
+	return nil
+}
+func (d *Database) Get(key []byte) ([]byte, error) {
+	return d.Normal[string(key)], nil
+}
