ca7035e93c97ebece0374a60f0f25244922d0840
Dominator008 <zhoumotongxue008@gmail.com>
2018-06-08 11:09:52 +0800

    db.go is done

--- playground/cmd/db.go
+++ playground/cmd/db.go
@@ -18,3 +18,19 @@ func (d *Database) PutHM(key []byte, args ...[]byte) error {
 func (d *Database) Get(key []byte) ([]byte, error) {
 	return d.Normal[string(key)], nil
 }
+
+func (d *Database) GetHM(key []byte, args ...[]byte) ([][]byte, error) {
+	key1 := string(key)
+	key2 := string(args[0])
+	return [][]byte{d.Normal[key1+"."+key2]}, nil
+}
+func (d *Database) Has(key []byte) (bool, error) {
+	_, ok := d.Normal[string(key)]
+	return ok, nil
+}
+func (d *Database) Delete(key []byte) error {
+	delete(d.Normal, string(key))
+	return nil
+}
+func (d *Database) Close() {
+}
