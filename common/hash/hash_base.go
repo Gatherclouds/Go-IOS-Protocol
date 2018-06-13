39c7d8b21913b931bb6715c026c7da002483bdc2
bitree8@gmail.com <wang.yannan@outlook.com>
2018-06-13 13:46:13 +0800

    method is inserted

--- common/hash/hash_base.go
+++ common/hash/hash_base.go
@@ -8,3 +8,19 @@ const (
 type Hash [HashLength]byte
 type LongHash [LongHashLength]byte
 
+func NewHash(bs []byte) Hash {
+	bLen := len(bs)
+	var h Hash
+
+	if bLen < HashLength {
+		for i, b := range bs {
+			h[HashLength-bLen+i] = b
+		}
+	} else {
+		for i := range h {
+			h[i] = bs[i]
+		}
+	}
+
+	return h
+}
