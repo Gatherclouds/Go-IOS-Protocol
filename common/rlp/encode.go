94e20dfd7ae47f92287d30c869bc3f0943435e82
deBreakr <wang.yannan@outlook.com>
2018-06-14 22:52:19 +0800

    a needless file has been removed

--- common/rlp/encode.go
+++ common/rlp/encode.go
@@ -11,6 +11,20 @@ type Encoder interface {
 	EncodeRLP(io.Writer) error
 }
 
+func Encode(w io.Writer, val interface{}) error {
+	if outer, ok := w.(*encbuf); ok {
+		return outer.encode(val)
+	}
+	eb := encbufPool.Get().(*encbuf)
+	defer encbufPool.Put(eb)
+	eb.reset()
+	if err := eb.encode(val); err != nil {
+		return err
+	}
+	return eb.toWriter(w)
+}
+
+
 // encode writes head to the given buffer, which must be at least
 // 9 bytes long. It returns the encoded bytes.
 func (head *listhead) encode(buf []byte) []byte {
