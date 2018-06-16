1584c8b5749aa0dcb29784f338f85ef863319f3f
tanky <itstangky@gmail.com>
2018-06-16 10:47:03 +0800

    this error is reported by DecodeBytes

--- common/rlp/decode.go
+++ common/rlp/decode.go
@@ -472,4 +472,9 @@ var (
 	ErrCanonSize      = errors.New("rlp: non-canonical size information")
 	ErrElemTooLarge   = errors.New("rlp: element is larger than containing list")
 	ErrValueTooLarge  = errors.New("rlp: value size exceeds available input length")
+
+	// This error is reported by DecodeBytes if the slice contains
+	// additional data after the first RLP value.
+	ErrMoreThanOneValue = errors.New("rlp: input contains more than one value")
+	
 	)
