45239f1399a3e8ce75b63be971ed803cba26124f
wangzhihaiSF <1599315665@qq.com>
2018-06-20 10:43:07 +0800

    add some functions

--- common/types.go
+++ common/types.go
@@ -12,3 +12,14 @@ func BytesToHash(b []byte) Hash {
 	h.SetBytes(b)
 	return h
 }
+
+func HexToHash(s string) Hash		{ return BytesToHash(FromHex(s)) }
+func (h Hash) Bytes() []byte { return h[:] }
+
+func (h *Hash) SetBytes(b []byte) {
+	if len(b) > len(h) {
+		b = b[len(b)-HashLength:]
+	}
+
+	copy(h[HashLength-len(b):], b)
+}
