8eff950f4ce7bf50bf991b72129d26d083f6d971
bitree8 <bitree8@gmail.com>
2018-06-20 10:46:38 +0800

    types.go is altered

--- common/types.go
+++ common/types.go
@@ -36,3 +36,9 @@ func IntToBytes(n int) []byte {
 	return bytesBuffer.Bytes()
 }
 
+func BytesToInt(b []byte) int {
+	bytesBuffer := bytes.NewBuffer(b)
+	var x int32
+	binary.Read(bytesBuffer, binary.BigEndian, &x)
+	return int(x)
+}
