281dd6fe7c9002f9e47c84b635a408b08a8dfc8f
He Pejian <glisxx6@gmail.com>
2018-06-20 11:11:54 +0800

    improve and perfect test support

--- common/types_test.go
+++ common/types_test.go
@@ -1,6 +1,16 @@
 package common
 
-import "math"
+import (
+	"math"
+	"testing"
+)
 
 var intCases = []int{-1, 0, 1, math.MaxInt32}
 var byteCases = [][]byte{{255, 255, 255, 255}, {0, 0, 0, 0}, {0, 0, 0, 1}, {127, 255, 255, 255}}
+
+func TestIntToBytes(t *testing.T) {
+	for k, v := range intCases {
+		bs := IntToBytes(v)
+		assert.Equal(t, byteCases[k], bs)
+	}
+}
